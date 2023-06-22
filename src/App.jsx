fd93e528a4faac274d1e4cbd9a8ae11c982c93a6
vitoormt <84139448+vitoormt@users.noreply.github.com>
2023-06-21 23:08:38 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='geral'>
+    <div className='container1'>
+      <h1>Feminicídio</h1>
+      <h2>Entenda sobre o assunto</h2>
+      
+      
+    </div>
+
+    <div className='container2'>
+    <h3>O que é o feminicídio?</h3>
+    <p>Feminicídio é um termo de crime de ódio baseado no gênero, mais definido como o assassinato de mulheres em violência doméstica ou em aversão ao gênero da vítima, mas as definições variam dependendo do contexto cultural.</p>
+    </div>
+
+    <div className='container3'>
+      <h3>Crescimento do numero de vitimas por ano</h3>
+      <p>
+              Entre 2015 e 2022, o feminicídio teve um crescimento alarmante globalmente.
+              O número de assassinatos de mulheres devido à sua condição de gênero aumentou
+              significativamente nesse período.Esse crescimento reflete a necessidade urgente
+              de ações efetivas para combater essa forma extrema de violência de gênero.
+            </p>
+    </div>
+
+    <div className='container4'>
+      <h3>Diferença no numero de vitimas pela cor de pele</h3>
+
+            <p>
+              Entre 2015 e 2022, o feminicídio teve um crescimento alarmante globalmente.
+              O número de assassinatos de mulheres devido à sua condição de gênero aumentou
+              significativamente nesse período.Esse crescimento reflete a necessidade urgente
+              de ações efetivas para combater essa forma extrema de violência de gênero.
+            </p>
+    </div>
+    <h3>Municipios com maior numerode de vitimas</h3>
+            <p>
+              O município de São Paulo tem registrado um dos maiores números de vítimas de feminicídio
+              no Brasil. A cidade enfrenta desafios significativos no combate a esse tipo de violência
+              de gênero, refletindo uma preocupação importante dentro do contexto urbano. Esforços têm
+              sido feitos para implementar medidas preventivas, leis de proteção e fortalecimento dos
+              sistemas de justiça, a fim de enfrentar esse problema grave. No entanto, é fundamental
+              que haja uma abordagem abrangente, envolvendo o engajamento da sociedade civil, governos
+              e instituições para garantir a segurança e a igualdade de direitos para todas as mulheres
+              no município de São Paulo.
+            </p>
+            <img src="https://cdn.discordapp.com/attachments/568273480303378433/1121258129024372896/Mapa-2.jpg" alt="" />
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default App
